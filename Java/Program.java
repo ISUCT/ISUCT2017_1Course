98c7e5baea4e16849d892b52b426d884b8ff2e20
Aventhor <vaster399@gmail.com>
2017-11-03 19:14:28 +0300

    Program for translating numeral systems(upd.last)

--- Java/Program.java
+++ Java/Program.java
@@ -11,40 +11,55 @@ public class Program {
 
     private void Start() {
         System.out.println("Выберите, что вы хотите сделать с числом:");
-        System.out.println("Для перевода из 10-ой в 2-ую, введите '1'");
+        System.out.println("Для перевода из 10-ой в n-ую, введите '1'");
         System.out.println("Для перевода из 2-ой в 10-ую, введите '2'");
         Scanner in = new Scanner(System.in);
-        int from10to2 = in.nextInt();
+        int transl = in.nextInt();
 
-        switch (from10to2) {
+        switch (transl) {
             case 1:
-                System.out.println("Введите число, которое хотите перевести");
+                System.out.println("(10->n)В какую систему счисления вы хотите перевести? (2,3,4,5,6,7,8,9,16)");
                 ArrayList<Integer> lib1 = new ArrayList<Integer>();
-                Scanner inp1 = new Scanner(System.in);
-                int x = inp1.nextInt();
-                while (x > 0)
-
-                {
-                    if (x % 2 == 0) {
-                        lib1.add(0);
-                        x /= 2;
-                    } else {
-                        lib1.add(1);
-                        x = (x - 1) / 2;
+                Scanner inp0 = new Scanner(System.in);
+                int whc = inp0.nextInt();
+                if (whc == 2) {
+                    System.out.println("(10->2)Введите число, которое хотите перевести:");
+                    Scanner inp1 = new Scanner(System.in);
+                    int x = inp1.nextInt();
+                    while (x > 0) {
+                        if (x % 2 == 0) {
+                            lib1.add(0);
+                            x /= 2;
+                        } else {
+                            lib1.add(1);
+                            x = (x - 1) / 2;
+                        }
+                    }
+                    for (int i = lib1.size() - 1; i >= 0; i--) {
+                        lib1.get(i);
+                        System.out.print(lib1.get(i) + " ");
                     }
                 }
-                for (
-                        int i = lib1.size() - 1;
-                        i >= 0; i--)
+                else if (whc == 3 || whc == 4|| whc == 5 || whc == 6 || whc == 7 || whc == 8 || whc == 9 || whc == 16) {
+                    System.out.println("(10->" + whc + ")Введите число, которое хотите перевести:");
+                    Scanner inp1 = new Scanner(System.in);
+                    int x = inp1.nextInt();
+                    int k = 0;
+                    while (x > 0) {
+                        k = x % whc;
+                        lib1.add(k);
+                        x /= whc;
+                    }
 
-                {
-                    lib1.get(i);
-                    System.out.print(lib1.get(i) + " ");
+                    for (int i = lib1.size() - 1; i >= 0; i--) {
+                        lib1.get(i);
+                        System.out.print(lib1.get(i) + " ");
+                    }
                 }
                 System.out.println(" ");
                 break;
             case 2:
-                System.out.println("Введите число, которое хотите перевести");
+                System.out.println("(2->10)Введите число, которое хотите перевести:");
                 ArrayList<Integer> lib2 = new ArrayList<Integer>();
                 Scanner inp2 = new Scanner(System.in);
                 int z = inp2.nextInt();
